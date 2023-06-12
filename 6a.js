ce54c57a5abace88c0a1abdcee3aaca4e886ca83
neha8978 <131157480+neha8978@users.noreply.github.com>
2023-06-12 14:14:11 +0530

    Add files via upload
    ex.6a

---
+++ 6a.js
@@ -0,0 +1,14 @@
+function testar()
+{
+    var m=10;
+    var message;
+    if(m%2==0)
+    {
+        message=("m is an even number");
+    }
+    else{
+        message=("m is not an even number");
+    }
+    document.write(message);
+}
+testar();
